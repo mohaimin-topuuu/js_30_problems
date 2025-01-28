dfce82295a1790313dd4ed1ea7190961680c153a
Mohaimin Topu <abmtopu73@gmail.com>
2025-01-28 17:38:03 +0600

    Count the repeated items in an array

---
+++ repeated_items.js
@@ -0,0 +1,34 @@
+//Count the repeated items in an array
+const myArr =  [5, 6, 11, 12, 98, 5, 5, 5];
+
+const find = 5;
+
+
+function myFunction(myArr) {
+
+    let count = 0;
+
+    for(const numbers of myArr) {
+
+        
+
+        if(numbers === find) {
+
+            count ++
+
+        }
+
+        
+    }
+
+    return count;
+
+    
+
+
+}
+
+const countedResult = myFunction(myArr);
+
+
+console.log(countedResult);
