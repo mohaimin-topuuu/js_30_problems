68d5f0159c96f834f9a4c1e4b65736467212b051
Mohaimin Topu <abmtopu73@gmail.com>
2025-01-28 17:11:41 +0600

    Farenheit to Celcius Convertion done

---
+++ farenheit_to_celcius.js
@@ -0,0 +1,18 @@
+// Write a function to convert farenheit to celcius 
+
+function farenheitToCelcius (temp) {
+    const celcius = temp * (9 / 5) + 32;
+
+
+    return celcius;
+
+
+
+}
+
+
+
+const convertedResult = farenheitToCelcius(-10)
+
+
+console.log(convertedResult);
