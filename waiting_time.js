a75ae6bac71b8c0993d65aa4b0073d732470346c
Mohaimin Topu <abmtopu73@gmail.com>
2025-01-30 14:27:25 +0600

    Waiitng time calculation

--- waiting_time.js
+++ waiting_time.js
@@ -1,3 +1,37 @@
+/*
+Israat is waiting for a job interview. His serial will actually be called to him.  The list of those who interviewed on a screen on the wall shows the list and how many minutes each one takes.  Israat does not understand how long it will take for him.  Your job is to create a function called Waitingtime () to calculate how much time it takes Israat.
+The function will take 2 input.  The first is the Array of a Number. Those who have completed the interview their interviewed time. The 2nd is Israat's serial number.  The function will find an average of the time for those whose interview is finished and find out how much time is needed to get to Israat's interview. The function will  return the needed time.  
+Input: Function will take 2 inputs,  
+One is Array (those who have siblings), the other is Israat's serial number.
+Serial Number > Length of Array
+
+Sample Input: [ 3, 5, 7, 11, 6 ], 10
+Sample Output: 24
+Sample Input: [13, 2], 6
+Sample Output: 24
+Sample Input: [13, 2, 6, 7, 10], 6
+Sample Output: 0
+Sample Input: [6], 4
+Sample Output: 12
+Sample Input: 7 , 10
+Sample Output: Invalid Input
+Sample Input: "[6,2]", 9
+Sample Output: Invalid Input
+Sample Input: [7, 8, 3, 4, 5], "9"
+Sample Output: Invalid Input
+
+
+*/
+
+
+
+
+
+
+
+
+
+
 function waitingTime(waitingTimes, serialNumber) {
     if(!Array.isArray(waitingTimes)) {
         return "Invalid Input";
@@ -17,3 +51,7 @@ function waitingTime(waitingTimes, serialNumber) {
         return requiredTime;
     }
 }
+
+const callFunction = waitingTime(waitingTimes, serialNumber);
+
+console.log(callFunction);
