6752712c57454b773c18c32b5e93c0b14b734e02
Mohaimin Topu <abmtopu73@gmail.com>
2025-01-30 14:05:07 +0600

    Check digits at username

--- check_digits_in_name.js
+++ check_digits_in_name.js
@@ -1,3 +1,29 @@
+/*
+*Raju has created a new website for passport registration.  Many people are making mistakes as well as different numbers while inputing the name. Which Raju does not want at all.  So you have to help Raju. You will write a function called Checkdigitsinname () that will take a string input and check if there is a number in the name. 
+If there is a number inside the name, then True will return. If there is no number, FALSE will return. If the input is not a string, the invalid input will return.
+Input: Raj123
+Output: true
+Input: n9ayeem
+Output: true
+Input: e1mu3
+Output: true
+Input: Suman
+Output: false
+Input: Name2024
+Output: true
+Input: !@#
+Output: false
+Input: ["Raj"]
+Output: Invalid Input
+
+
+
+
+*/
+
+
+
+
 function checkDigitsInName(userString) {
     for (let char of userString) {
         if(char >= '0' && char <= '9') {
@@ -9,3 +35,8 @@ function checkDigitsInName(userString) {
     }
     return false;
 }
+
+
+const callFunction = checkDigitsInName(userString);
+
+console.log(callFunction);
